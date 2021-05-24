700a3165283026c9c9f1e8a042531d1370efda51
LauraJimenezB <laura271260@gmail.com>
2021-05-24 17:08:22 -0500

    test toLowerCase

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -19,3 +19,16 @@ test('returns an integer from type string', () => {
   const actual = isInteger(stringValue);
   expect(actual).toEqual(false);
 });
+
+// Test: toLowerCase
+test('if string parameter is empty get message', () => {
+  const emptyString = '';
+  const actual = toLowerCase(emptyString);
+  expect(actual).toEqual('Please provide a string');
+});
+
+test('string parameter transforms to lower case', () => {
+  const stringParameter = 'hELlo';
+  const actual = toLowerCase(stringParameter);
+  expect(actual).toEqual('hello');
+});
